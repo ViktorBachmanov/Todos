735dbd4c90cbeb98b84e079c38932d86bbfca815
Vikt0r <vbachmanov@mail.ru>
2022-02-14 14:31:45 +0300

    Dark mode on

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export default function AppBar() {
+    return (
+        <div className='dark:bg-slate-800'>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Todos</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
