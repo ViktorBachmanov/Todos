e6c9fc549bec05e82311cbe6a28ebc4ecc39abc6
Vikt0r <vbachmanov@mail.ru>
2022-02-14 21:03:59 +0300

    Add "complete" Chip component

---
+++ src/components/Chip.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Chip() {
+  return <span className="Chip">Completed</span>;
+}
--- src/features/todos/Card.tsx
+++ src/features/todos/Card.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Todo } from "./types";
+import Chip from "../../components/Chip";
 
 type Props = {
   todo: Todo;
 };
 
 export default function Card(props: Props) {
-  return <div className="Card">{props.todo.title}</div>;
+  const { title, completed } = props.todo;
+
+  return (
+    <div className="Card">
+      <div>{title}</div>
+      {completed && <Chip />}
+    </div>
+  );
 }
