df80140ab31c9df3a51f85ae1fd8053d6a3094f1
Vikt0r <vbachmanov@mail.ru>
2022-02-14 19:18:11 +0300

    Add Redux todosSlice

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import { connect, ConnectedProps } from "react-redux";
+//import { RootState } from "../app/store";
+import { fetchTodos as fetchTodosAction } from "../features/todos/todosSlice";
 import AppBar from "./AppBar";
 
-export default function Layout() {
+const mapDispatchToProps = {
+  fetchTodos: fetchTodosAction,
+};
+
+const connector = connect(null, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+function Layout(props: PropsFromRedux) {
+  useEffect(() => {
+    console.log("Layout useEffect()");
+
+    props.fetchTodos();
+  }, []);
+
   return (
     <div className="Layout dark:bg-zinc-900">
       <AppBar />
@@ -11,3 +28,5 @@ export default function Layout() {
     </div>
   );
 }
+
+export default connector(Layout);
--- src/features/todos/Todos.tsx
+++ src/features/todos/Todos.tsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { connect, ConnectedProps } from "react-redux";
+import { RootState } from "../../app/store";
 
-export default function Todos() {
+function mapStateToProps(state: RootState) {
+  return {
+    todos: state.todos.list,
+    status: state.todos.status,
+  };
+}
+
+const connector = connect(mapStateToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+function Todos(props: PropsFromRedux) {
+  if (props.status === "loading") {
+    return <h2>Loading...</h2>;
+  }
+
   return (
-    <div>
-      <h2 className="dark:text-slate-300">Todos</h2>
+    <div className="dark:text-slate-300">
+      <h2>Todos</h2>
+      {props.todos.map((todo) => {
+        return <div key={todo.id}>{todo.title}</div>;
+      })}
     </div>
   );
 }
+
+export default connector(Todos);
